699bb70bcfac9ded202f680a8821f5c180e1e7b2
Zayyad <Zayyadidris294@gmail.com>
2020-09-07 14:43:40 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template
 from wtforms_fields import *
-
+from models import *
 
 app = Flask(__name__)
 app.secret_key = 'Idontfuckingcare'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://ytdbqumpjfscjt:4b4e292afc9f625ba372a26ce5dde8b7020df316e91a4f81faea27a93accbb3e@ec2-54-147-54-83.compute-1.amazonaws.com:5432/dnbg4eods0163'
+
+db = SQLAlchemy(app)
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
     
     reg_form = RegistrationForm()
 
     if reg_form.validate_on_submit():
-        return "Great success"
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        user_object = User.query.filter_by(username=username).first()
+        if user_object:
+            return "someone else has taken this username!"
+
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return "Inserted into DB!"
 
     return render_template("index.html", form=reg_form)
 
--- models.py
+++ models.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = "polaruser"
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    password = db.column(db.String(), nullable=False)
+    password = db.column(db.String())
 
